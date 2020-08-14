84eacb60e53af9614ca6e611934daab0d8d7e0fd
Michal Knapczyk <michalkna01@gmail.com>
2020-08-14 22:53:45 +0200

    Added new links in projects tab

--- contact.php
+++ contact.php
@@ -4,6 +4,7 @@
     $_SESSION['error']="";
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="pl-PL">
   <head>
@@ -21,10 +22,13 @@
   </head>
   <body>
     <div id="main">
+
       <div id="top">
+
         <div class="logo">
           <p>Michał Knapczyk</p>
         </div>
+
         <div class="panel">
           <ul>
             <a href="index.html"><li>O mnie</li></a>
@@ -32,15 +36,20 @@
             <a href="contact.php"><li>Kontakt</li></a>
           </ul>
         </div>
+
       </div>
+
       <div id="content">
+
         <div class="left">
           <h1>Formularz kontaktowy</h1>
           <p>
             Masz pytanie lub ofertę? Skontaktuj się ze mną poprzez formularz:
           </p>
         </div>
+
         <div class="left">
+
           <form method="post" action="send.php">
             <input
               type="email"
@@ -60,6 +69,7 @@
             <button type="reset">Wyczyść &#10007;</button>
             <button type="submit">Wyślij &#10003;</button>
           </form>
+
           <span>
             <?php
                 echo $_SESSION['error'];
@@ -68,12 +78,16 @@
                 }
             ?>
           </span>
+
         </div>
+
       </div>
+
       <footer>
         <p><a href="#main">Powrót do góry strony</a></p>
         <p>&copy; 2020</p>
       </footer>
+
     </div>
   </body>
 </html>
